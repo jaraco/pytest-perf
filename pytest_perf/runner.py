46a4c62da1fa7c0b6da96d2d0049a2878d123ce6
Jason R. Coombs <jaraco@jaraco.com>
2021-06-20 18:21:52 -0400

    Now runner.Result calculates the difference between experiment and control and renders the difference in its text representation.

--- pytest_perf/runner.py
+++ pytest_perf/runner.py
@@ -4,6 +4,7 @@ import subprocess
 import contextlib
 
 import pip_run
+import tempora
 
 
 _text = dict(text=True) if sys.version_info > (3, 7) else dict(universal_newlines=True)
@@ -23,15 +24,42 @@ class Command(list):
 
 
 class Result:
-    def __init__(self, baseline, local):
-        self.baseline_text = baseline
-        self.local_text = local
+    # by default, anything under 100% increase is not significant
+    tolerance = 1.0
 
+    def __init__(self, control, experiment):
+        self.control_text = control
+        self.experiment_text = experiment
+
+    @property
     def delta(self):
-        return self.local - self.baseline
+        return self.experiment - self.control
+
+    @property
+    def variance(self):
+        return self.delta / self.control
+
+    @property
+    def significant(self):
+        return self.variance > self.tolerance
+
+    @property
+    def experiment(self):
+        return self._parse_timeit_duration(self.experiment_text)
+
+    @property
+    def control(self):
+        return self._parse_timeit_duration(self.control_text)
+
+    @staticmethod
+    def _parse_timeit_duration(time):
+        return tempora.parse_timedelta(time)
+
+    def __str__(self):
+        return f'{self.experiment} (+{self.delta}, {self.variance:.0%})'
 
     def __repr__(self):
-        return f'Result({self.baseline_text!r}, {self.local_text!r})'
+        return f'Result({self.control_text!r}, {self.experiment_text!r})'
 
 
 class BenchmarkRunner:
