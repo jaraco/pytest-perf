e82753529b659601602cdf5e7e4484f87565fc13
Jason R. Coombs <jaraco@jaraco.com>
2024-01-28 11:27:45 -0500

    Suppress UnicodeDecodeError when a non-standard encoding is encountered.
    Closes #9.

--- pytest_perf/plugin.py
+++ pytest_perf/plugin.py
@@ -43,7 +43,8 @@ def _collect_file_pytest6(parent, path):
 
 
 old_pytest = Version(pytest.__version__) < Version('7')
-pytest_collect_file = _collect_file_pytest6 if old_pytest else _collect_file_pytest7
+pytest_collect_file = suppress(UnicodeDecodeError)(
+    _collect_file_pytest6 if old_pytest else _collect_file_pytest7)
 
 
 def pytest_terminal_summary(terminalreporter, config):
